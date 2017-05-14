24964fd1177d067888cd0411b9154dd0a25f6d12
LefkaLefka <lefka.velchenko@gmail.com>
2017-05-14 20:41:35 +0300

    function fo rclik logic

--- Pairs/Game.h
+++ Pairs/Game.h
@@ -2,6 +2,7 @@
 #include <list>
 #include <iostream>
 #include <stdlib.h>
+#include "GameData.h"
 
 using namespace System;
 
@@ -18,6 +19,9 @@ public:
 		this->setImagesPath();
 	}
 	~Game() { }
+	// Control array	
+	//int controlArray[4][3];
+	int** controlArray;
 	void setImagesPath()
 	{
 		array<String^>^ files = System::IO::Directory::GetFiles(System::Environment::CurrentDirectory + "\\img\\content\\", "*.jpg");
@@ -67,9 +71,11 @@ public:
 		}
 		for (int i = 0; i < this->COUNT_OF_GAME_IMAGES * 2; ++i)
 		{
+			// index -> [i][j]
 			int buff = Math::Floor(i / 3.0);
 			this->controlArray[buff][i - 3 * buff] = temp[i];
 		}
+		this->staredtGame = true;
 	}
 	int** getControlArray()
 	{
@@ -79,14 +85,79 @@ public:
 	{
 		return this->pathImagesInGame;
 	}
+	struct _resultClick click(String^ objectNumber)
+	{		
+		// IF STARTED GAME
+		if (this->staredtGame)
+		{
+			// IF FIRST CLICK ON IMAGE
+			if (this->openedImage == -1)
+			{
+				this->openedImage = Convert::ToInt16(objectNumber);
+				int ic = Math::Floor(this->openedImage / 3.0);
+				int jc = this->openedImage - 3 * ic;
+				// If we click on empty image
+				if (this->controlArray[ic][jc] == -1) 
+				{
+					// Nothing to show
+					resultClick = { -1, -1, false, false, 0 };
+					this->openedImage = -1;
+					return resultClick;
+				}
+				// Show only first image
+				resultClick = { this->openedImage, -1, true, false, 0 };
+				return resultClick;
+			}
+			// IF SECOND CLICK
+			else
+			{
+				int index = Convert::ToInt16(objectNumber);
+				// If not one and the same pictures
+				if (index != this->openedImage)
+				{
+					// INDEX -> [I][J]
+					// Previus image indexes
+					int ip = Math::Floor(this->openedImage / 3.0);
+					int jp = this->openedImage - 3 * ip;
+					// Current image indexes
+					int ic = Math::Floor(index / 3.0);
+					int	jc = index - 3 * ic;
+					// Check if previous image = current image
+					if (this->controlArray[ip][jp] == this->controlArray[ic][jc])
+					{
+						// Show first and second image
+
+						// Delete images
+						//this->controlArray[ip][jp] = -1;
+						//this->controlArray[ic][jc] = -1;
+
+						resultClick = { this->openedImage, index, true, true, 0 };
+						this->openedImage = -1;
+						return resultClick;
+					}
+					// Check if previous image != current image
+					else
+					{						
+						// Show first and second image to 5 second
+						resultClick = { this->openedImage, index, true, true, 5 };
+						this->openedImage = -1;
+						return resultClick;
+					}
+				}
+			}
+		}
+		else
+		{
+			// Nothing to show
+			resultClick = { -1, -1, false, false, 0 };
+			return resultClick;
+		}
+	}
 private:
 	// All images in directory
 	std::string* pathImagesArray;
 	// Path to image in current game
 	std::string* pathImagesInGame;
-	// Control array	
-	//int controlArray[4][3];
-	int** controlArray;
 	// Count of images for width
 	const int WIDTH_CONTROL_ARRAY = 3;
 	// Count of images for height
@@ -94,6 +165,9 @@ private:
 	// Count of image in game
 	const int COUNT_OF_GAME_IMAGES = 6;
 	int COUNT_OF_ALL_IMAGES;
+	int openedImage = -1;
+	bool staredtGame = false;
+	_resultClick resultClick;
 	void MarshalString(String^ s, std::string& os) {
 		using namespace Runtime::InteropServices;
 		const char* chars =
